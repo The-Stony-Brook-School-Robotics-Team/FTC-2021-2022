255ed4269c454e94afcc52c5612a56a3e710dfea
Bears-Console <90263953+Bears-Console@users.noreply.github.com>
2021-09-28 10:24:46 -0400

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Max/PurePursuitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Max/PurePursuitTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Sandboxes.Max;
 
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.arcrobotics.ftclib.kinematics.HolonomicOdometry;
@@ -20,11 +21,24 @@ public class PurePursuitTest extends LinearOpMode {
     private Motor.Encoder RightEncoder;
     private Motor.Encoder CentralEncoder;
     private HolonomicOdometry odometry;
-    private MotorEx lf = new MotorEx(hardwareMap, "lf"), rf = new MotorEx(hardwareMap, "leftodom"), lb = new MotorEx(hardwareMap, "backodom"), rb = new MotorEx(hardwareMap, "rightodom");
+
+
+    private MotorEx lf;
+    private MotorEx rf;
+    private MotorEx lb;
+    private MotorEx rb;
+    private MecanumDrive driveTrain;
 
     private final double DistancePerPulse = Math.PI*2.0/8192;
     @Override
     public void runOpMode() throws InterruptedException {
+         lf = new MotorEx(hardwareMap, "lf");
+         rf = new MotorEx(hardwareMap, "leftodom");
+         lb = new MotorEx(hardwareMap, "backodom");
+         rb = new MotorEx(hardwareMap, "rightodom");
+
+        driveTrain = new MecanumDrive(lf, rf, lb, rb);
+
         LeftEncoder = rf.encoder.setDistancePerPulse(DistancePerPulse);
         RightEncoder = rb.encoder.setDistancePerPulse(DistancePerPulse);
         CentralEncoder = lb.encoder.setDistancePerPulse(DistancePerPulse);
@@ -42,8 +56,10 @@ public class PurePursuitTest extends LinearOpMode {
         //Waypoint premW = new InterruptWaypoint(8192*2, 8192*2, odometry.updatePose()); //Learning "Position Buffer"
         Waypoint intermediateW= new GeneralWaypoint(8192*10, 8192*10, Math.PI*2, 1,1, 2*Math.PI);
         Waypoint postW = new InterruptWaypoint();
-        Waypoint endW = new EndWaypoint();
-        Path testP = new Path(startW,intermediateW);
+        Waypoint endW = new EndWaypoint(8192*11, 8192*11, Math.PI*2, 1, 1, 2*Math.PI*2, 2*Math.PI*2, 2*Math.PI*2);
+        Path testP = new Path(startW, intermediateW, endW);
         testP.init();
+        testP.followPath(driveTrain, odometry);
+
     }
 }
