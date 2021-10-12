96aee4811e2420197d4159751dbb45b010b75c22
MAXKEHANMAX <kehan.cao@sbs.org>
2021-10-12 09:43:49 -0400

    Update BallCollecting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Max/BallCollecting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Max/BallCollecting.java
@@ -2,19 +2,29 @@ package org.firstinspires.ftc.teamcode.Sandboxes.Max;
 
 
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
 
 @TeleOp
-public class BallCollecting extends Liner {
+public class BallCollecting extends LinearOpMode {
     private MotorEx Motor;
+    private MotorEx MotorEncoder;
 
     @Override
-    public void runOpMode throws InterruptedException{
+    public void runOpMode() throws InterruptedException{
 
+Motor = new MotorEx(hardwareMap, "1");
+MotorEncoder = new MotorEx(hardwareMap, "1");
 
+waitForStart();
+while(true){
 
+    Motor.setVelocity(gamepad1.left_stick_y);
+    telemetry.addData("Velocity: ",MotorEncoder.getVelocity());
+    telemetry.update();
+}
 
     }
 }
