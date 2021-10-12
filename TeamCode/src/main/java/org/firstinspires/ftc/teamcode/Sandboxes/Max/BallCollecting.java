0204d6efa51fbf5b8d839be14af4bc06730c58b6
Dennis Yilmaz <dy.0x8@outlook.com>
2021-10-12 10:51:55 -0400

    Update BallCollecting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Max/BallCollecting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Max/BallCollecting.java
@@ -15,6 +15,8 @@ public class BallCollecting extends LinearOpMode {
     private boolean pressingArrowDown = false;
     private double voltage = 0;
 
+    private boolean pressingA = false;
+
     @Override
     public void runOpMode() throws InterruptedException{
         Motor = hardwareMap.get(DcMotor.class, "m1");
@@ -38,6 +40,21 @@ public class BallCollecting extends LinearOpMode {
 
             if(gamepad1.left_stick_y < 0.1) {
                 voltage = gamepad1.left_stick_y;
+            }
+
+            if(gamepad1.a && !pressingA) {
+                pressingA = true;
+            } else if(!gamepad1.a && pressingA) {
+                // Friction
+                // TODO: Test this constant
+                double Mews = 0.1;
+
+                double sqrtOfR;
+                double squareResult = 1 / (Math.sqrt(Mews));
+
+
+
+                pressingA = false;
             }
 
             Motor.setPower(voltage);
