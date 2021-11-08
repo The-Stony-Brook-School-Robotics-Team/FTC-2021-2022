311b7397d010bb291add764ef18fbc67ff70fe0c
Bears-Console <90263953+Bears-Console@users.noreply.github.com>
2021-11-08 14:53:13 -0500

    add changes to odom follower rather than cam

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForthDefault.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForthDefault.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.BearsUtil.T265Controller;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveOdom;
 
 /*
  * Op mode for preliminary tuning of the follower PID coefficients (located in the drive base
@@ -33,7 +34,7 @@ public class BackAndForthDefault extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        SampleMecanumDriveOdom drive = new SampleMecanumDriveOdom(hardwareMap);
 
         Trajectory trajectoryForward = drive.trajectoryBuilder(new Pose2d())
                 .forward(DISTANCE)
