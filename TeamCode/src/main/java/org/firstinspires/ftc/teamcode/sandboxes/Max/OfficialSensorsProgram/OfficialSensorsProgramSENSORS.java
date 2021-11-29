7ce8c80258e47ef0789d9ad56f4ef574154033e5
MAXKEHANMAX <kehan.cao@sbs.org>
2021-11-29 14:56:33 -0500

    Sensors Official Website Program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/Max/OfficialSensorsProgram/OfficialSensorsProgramSENSORS.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.sandboxes.Max.OfficialSensorsProgram;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name = "Range Sensors", group = "MRI")
+public class OfficialSensorsProgramSENSORS extends OpMode{
+    private ElapsedTime runtime = new ElapsedTime();
+
+    byte[] rangeAcache;
+    byte[] rangeCcache;
+
+    I2cDevice rangeA;
+    I2cDevice rangeC;
+    I2cDeviceSynch rangeAreader;
+    I2cDeviceSynch rangeCreader;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        rangeA = hardwareMap.i2cDevice.get("UltrasonicSensor");
+        rangeC = hardwareMap.i2cDevice.get("ColourSensor");
+
+        rangeAreader = new I2cDeviceSynchImpl(rangeA, I2cAddr.create8bit(0x28), false);
+        rangeCreader = new I2cDeviceSynchImpl(rangeC, I2cAddr.create8bit(0x2a), false);
+
+        rangeAreader.engage();
+        rangeCreader.engage();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Running: " + runtime.toString());
+
+        rangeAcache = rangeAreader.read(0x04, 2);  //Read 2 bytes starting at 0x04
+        rangeCcache = rangeCreader.read(0x04, 2);
+
+        int RUS = rangeCcache[0] & 0xFF;   //Ultrasonic value is at index 0. & 0xFF creates a value between 0 and 255 instead of -127 to 128
+        int LUS = rangeAcache[0] & 0xFF;
+        int RODS = rangeCcache[1] & 0xFF;
+        int LODS = rangeAcache[1] & 0xFF;
+
+        //display values
+        telemetry.addData("1 A US", LUS);
+        telemetry.addData("2 A ODS", LODS);
+        telemetry.addData("3 C US", RUS);
+        telemetry.addData("4 C ODS", RODS);
+    }
+}
