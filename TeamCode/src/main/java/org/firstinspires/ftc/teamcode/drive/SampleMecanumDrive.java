cdbf2e59a7eef4cf939cfd343a696883508679d9
Bears-Console <90263953+Bears-Console@users.noreply.github.com>
2021-11-30 10:55:18 -0500

    Transfer PID
    Transfer PID from CustomTimeoutTuningDrive to SampleMecanumDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -51,8 +51,8 @@ import java.util.List;
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
 
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(43, 26.4, 46.54); // 60 0 1
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(100, 0, 3); // 40 0 0
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(62, 2, 3);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(9, 0, .03);
 
 
     public static double LATERAL_MULTIPLIER = 1;
