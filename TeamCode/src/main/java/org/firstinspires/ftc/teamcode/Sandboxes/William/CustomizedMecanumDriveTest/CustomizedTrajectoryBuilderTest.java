6c8f6bb1f3b105122177970745a19ea5a8329067
William Tao <williamtao345@gmail.com>
2021-12-10 16:55:27 -0500

    Updated Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/William/CustomizedMecanumDriveTest/CustomizedTrajectoryBuilderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/William/CustomizedMecanumDriveTest/CustomizedTrajectoryBuilderTest.java
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-package org.firstinspires.ftc.teamcode.sandboxes.William.Tuning;
-=======
 package org.firstinspires.ftc.teamcode.sandboxes.William.CustomizedMecanumDriveTest;
->>>>>>> Stashed changes
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -10,15 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.sandboxes.William.Util.CustomizedMecanumDrive;
-<<<<<<< Updated upstream
-=======
-import org.firstinspires.ftc.teamcode.common.tuning.timeout.TurnTest;
-import org.firstinspires.ftc.teamcode.drive.timeout.CustomTimeoutTuningDrive;
->>>>>>> Stashed changes
 
 @Config
-@Autonomous(group = "drive", name = "Customized Mecanum Drive Test")
-public class CustomizedMecanumDriveTest extends LinearOpMode {
+@Autonomous(group = "drive", name = "Customized Trajectory Builder Test")
+public class CustomizedTrajectoryBuilderTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
