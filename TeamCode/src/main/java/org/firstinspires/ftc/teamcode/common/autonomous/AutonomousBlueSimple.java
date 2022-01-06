50184c5d2475b781eb550f8cb246262ab59200e0
Astro Kid <71262643+MDNich@users.noreply.github.com>
2022-01-06 12:30:55 -0500

    autonomi are ready to go: need to run and test and add hardware for slide etc.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousBlueSimple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousBlueSimple.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.common.autonomous;
 
-public class AutonomousBlueSimple {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name="A - Auton (Blue Simple)")
+public class AutonomousBlueSimple extends OpMode {
+    AutonomousBrain brain;
+
+    @Override
+    public void init() {
+        brain = new AutonomousBrain(hardwareMap,telemetry,AutonomousMode.BlueSimple);
+    }
+
+    @Override
+    public void start() {
+        brain.launch();
+    }
+
+    @Override
+    public void loop() {
+        brain.doAutonAction();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousRedSimple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousRedSimple.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.common.autonomous;
 
-public class AutonomousRedSimple {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+@Autonomous(name="A - Auton (Red Simple)")
+public class AutonomousRedSimple extends OpMode {
+    AutonomousBrain brain;
+
+    @Override
+    public void init() {
+        brain = new AutonomousBrain(hardwareMap,telemetry,AutonomousMode.RedSimple);
+    }
+
+    @Override
+    public void start() {
+        brain.launch();
+    }
+
+    @Override
+    public void loop() {
+        brain.doAutonAction();
+    }
 }
