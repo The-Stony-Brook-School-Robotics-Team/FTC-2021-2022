b88b28f037c3064b71e96cb909ebdc91b37ad6d2
Dennis Yilmaz <dy.0x8@outlook.com>
2022-01-11 10:24:32 -0500

    Create SlideDebugging.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Dennis/SlideDebugging.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.Sandboxes.Dennis;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Sandboxes.Michael.Unsafe.SlideController;
+
+@TeleOp(name = "T - Slide Debugger", group = "default")
+public class SlideDebugging extends LinearOpMode {
+
+    private static int limitMin = 0;
+    private static double PosLimitThreshold = 0.05;
+    private static double NegLimitThreshold = 0.05;
+    private SlideController slideController;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        slideController = new SlideController(hardwareMap, telemetry);
+
+        waitForStart();
+
+        while(!isStopRequested()) {
+            limitMin = slideController.getMotorPosition();
+            if(gamepad1.right_stick_x != 0 && gamepad1.right_stick_x > PosLimitThreshold && gamepad1.right_stick_x < NegLimitThreshold) {
+                if(slideController.getMotorPosition() > limitMin) {
+                    slideController.setMotorIncrement((int)gamepad1.right_stick_x);
+                }
+
+            }
+
+            telemetry.addData("Slide Encoder Pos: ", slideController.getMotorPosition());
+            telemetry.update();
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
