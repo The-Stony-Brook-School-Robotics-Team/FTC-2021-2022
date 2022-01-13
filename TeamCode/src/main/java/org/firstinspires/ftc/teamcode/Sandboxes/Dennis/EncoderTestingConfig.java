1195ddfea2bfefc181496b37d34c29fd70d4e229
Dennis Yilmaz <dy.0x8@outlook.com>
2022-01-13 11:17:00 -0500

    asd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Dennis/EncoderTestingConfig.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Sandboxes.Dennis;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class EncoderTestingConfig {
+    public static double setPower = 0;
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Dennis/EnoderTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Dennis/EnoderTesting.java
@@ -1,37 +1,71 @@
 package org.firstinspires.ftc.teamcode.Sandboxes.Dennis;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Config
 @TeleOp(name="XXX - Encoder Testing", group = "default")
 public class EnoderTesting extends LinearOpMode {
 
     private static DcMotor spoolMotor;
 
+    private static double currentPower = 0;
+
+    private static boolean stopRequested = false;
+
     @Override
     public void runOpMode() throws InterruptedException {
         spoolMotor = hardwareMap.dcMotor.get("spool");
         spoolMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        spoolMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        powerUpdater.start();
+
 
         waitForStart();
 
-
+        spoolMotor.setPower(currentPower);
+        runToPosition(0);
 
         while(!isStopRequested()) {
-            spoolMotor.setTargetPosition(800);
-//            spoolMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            spoolMotor.setTargetPosition(0);
-            spoolMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            stopRequested = isStopRequested();
+
+            runToPosition(800);
             while (spoolMotor.isBusy()) {
                 telemetry.addData("pos: ", spoolMotor.getCurrentPosition());
                 telemetry.update();
             }
 
+            runToPosition(0);
+            while (spoolMotor.isBusy()) {
+                telemetry.addData("pos: ", spoolMotor.getCurrentPosition());
+                telemetry.update();
+            }
+
+
+
         }
 
     }
 
+    private static Thread powerUpdater = new Thread(() -> {
+        while (!stopRequested) {
+            if(EncoderTestingConfig.setPower != currentPower) {
+                if(!spoolMotor.isBusy()) {
+                    spoolMotor.setPower(EncoderTestingConfig.setPower);
+                }
+            }
+        }
+    });
 
+    private static void runToPosition(int ticks) {
+        if(spoolMotor.getPower() == 0) {
+            spoolMotor.setPower(0.3);
+        }
+        spoolMotor.setTargetPosition(ticks);
+        spoolMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
 
 }
