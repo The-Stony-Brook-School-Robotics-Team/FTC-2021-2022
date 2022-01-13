b47633334f44f53fe1573bd41f1df79c0b4d6ca5
Astro Kid <71262643+MDNich@users.noreply.github.com>
2022-01-13 10:14:15 -0500

    fixed slide controller!

--- TeamCode/src/main/java/org/sbs/bears/robotframework/controllers/SlideController.java
+++ TeamCode/src/main/java/org/sbs/bears/robotframework/controllers/SlideController.java
@@ -1,12 +1,17 @@
 package org.sbs.bears.robotframework.controllers;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.sbs.bears.robotframework.enums.SlideComponents;
+import org.sbs.bears.robotframework.enums.SlideState;
+import org.sbs.bears.robotframework.enums.SlideTarget;
 
 public class SlideController {
     private Servo verticalServo;
@@ -17,6 +22,10 @@ public class SlideController {
 
 
     // TODO: determine which states are necessary and create variables and state machine
+
+    SlideState slideState = SlideState.PARKED; // low-level steps
+    SlideTarget targetParams = SlideTarget.NA;
+    private boolean flagToLeave = false;
 
 
     public SlideController(HardwareMap hardwareMap, Telemetry telemetry)
@@ -34,6 +43,162 @@ public class SlideController {
         slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
+    public void extendDropRetract(SlideTarget target)
+    {
+        this.targetParams = target;
+        extendSlide();
+        if(flagToLeave) {
+            return;
+        }
+        dropCube();
+        retractSlide();
+        this.targetParams = SlideTarget.NA;
+    }
+
+    private void dropCube()
+    {
+        if(slideState == SlideState.OUT_FULLY) {
+            dumperServo.setPosition(dumperPosition_DUMP);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            dumperServo.setPosition(dumperPosition_HOLDBLOCK);
+        }
+        else {
+            Log.d("SlideController","Assert for slide out fully failed; dont release the cube in transit!");
+        }
+    }
+
+    private void retractSlide() {
+        slideState = SlideState.RET_BUCKET_OUT;
+        doStateAction();
+        slideState = SlideState.RET_BUCKET_IN;
+        doStateAction();
+        slideState = SlideState.PARKED;
+    }
+
+    private void extendSlide() {
+        slideState = SlideState.EXT_BUCKET_IN;
+        doStateAction();
+        slideState = SlideState.EXT_BUCKET_OUT;
+        doStateAction();
+        if(flagToLeave)
+        {
+            slideState = SlideState.RET_BUCKET_IN;
+            doStateAction();
+            slideState = SlideState.PARKED;
+            return;
+        }
+        slideState = SlideState.OUT_FULLY;
+    }
+
+    // this method will execute tasks associated with a transition state such as extending and retracting.
+    // position states such as parked and out fully have no associated operations.
+    private void doStateAction()
+    {
+        int targetPos = 0;
+        int targetPosFinal = 0;
+        double verticalServoTargetPos = 0;
+        switch(slideState) {
+            case PARKED:
+                return;
+            case EXT_BUCKET_IN:
+                targetPos = slideMotorPosition_BUCKET_OUT;
+                slideMotor.setTargetPosition(targetPos);
+                while (slideMotor.getCurrentPosition() < targetPos) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return;
+            case EXT_BUCKET_OUT:
+                switch (targetParams) {
+                    case ONE_CAROUSEL:
+                        targetPosFinal = slideMotorPosition_ONE_CAROUSEL;
+                        verticalServoTargetPos = vertServoPosition_ONE_CAROUSEL;
+                        break;
+                    case TWO_CAROUSEL:
+                        targetPosFinal = slideMotorPosition_TWO_CAROUSEL;
+                        verticalServoTargetPos = vertServoPosition_TWO_CAROUSEL;
+                        break;
+                    case THREE_CAROUSEL:
+                        targetPosFinal = slideMotorPosition_THREE_CAROUSEL;
+                        verticalServoTargetPos = vertServoPosition_THREE_CAROUSEL;
+                        break;
+                    case THREE_DEPOSIT:
+                        targetPosFinal = slideMotorPosition_THREE_DEPOSIT;
+                        verticalServoTargetPos = vertServoPosition_THREE_DEPOSIT;
+                        break;
+                    case NA:
+                        Log.d("SlideController","Slide Extension failed: did not specify target. Exiting");
+                        flagToLeave = true;
+                        return;
+                }
+                slideMotor.setTargetPosition(targetPosFinal);
+                setHeightToParams(verticalServoTargetPos);
+                while (slideMotor.getCurrentPosition() < targetPosFinal) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return;
+            case OUT_FULLY:
+                return;
+            case RET_BUCKET_OUT:
+                targetPos = slideMotorPosition_BUCKET_OUT;
+                slideMotor.setTargetPosition(targetPos);
+                setHeightToParams(vertServoPosition_PARKED);
+                while (slideMotor.getCurrentPosition() > targetPos) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return;
+            case RET_BUCKET_IN:
+                targetPos = slideMotorPosition_PARKED;
+                slideMotor.setTargetPosition(targetPos);
+                while (slideMotor.getCurrentPosition() > targetPos) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return;
+        }
+    }
+
+
+    private void setHeightToParams(double targetPos) {
+        if(slideState == SlideState.OUT_FULLY || slideState == SlideState.RET_BUCKET_OUT || slideState == SlideState.EXT_BUCKET_OUT) {
+            double currentPos = verticalServo.getPosition();
+            boolean qNeedsToGoUp = (currentPos < targetPos);
+            if(qNeedsToGoUp) {
+                for(double i = verticalServo.getPosition(); i < targetPos; i+=incrementDelta){
+                    verticalServo.setPosition(Range.clip(i, targetPos, 1));
+                }
+            }
+            else {
+                for(double i = verticalServo.getPosition(); i > targetPos; i-=incrementDelta){
+                    verticalServo.setPosition(Range.clip(i, targetPos, 0));
+                }
+            }
+        }
+        else {
+            Log.d("SlideController","Assert for slide box outside of robot failed; dont lift the slide while the box is inside the robot!");
+        }
+    }
+
+
+
     // TODO MEASURE ALL CONSTANTS
 
     // vertical servo
@@ -49,12 +214,12 @@ public class SlideController {
     double dumperPosition_HOLDBLOCK = 0;
 
     // slide motor
-    double slideMotorPosition_PARKED = 0;
-    double slideMotorPosition_BUCKET_OUT = 0; // minimum position for the bucket to be out
-    double slideMotorPosition_THREE_DEPOSIT = 0;
-    double slideMotorPosition_THREE_CAROUSEL = 0;
-    double slideMotorPosition_TWO_CAROUSEL = 0;
-    double slideMotorPosition_ONE_CAROUSEL = 0;
+    int slideMotorPosition_PARKED = 0;
+    int slideMotorPosition_BUCKET_OUT = 0; // minimum position for the bucket to be out
+    int slideMotorPosition_THREE_DEPOSIT = 0;
+    int slideMotorPosition_THREE_CAROUSEL = 0;
+    int slideMotorPosition_TWO_CAROUSEL = 0;
+    int slideMotorPosition_ONE_CAROUSEL = 0;
 
 
 
