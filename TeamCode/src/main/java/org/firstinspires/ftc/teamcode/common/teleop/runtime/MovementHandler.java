20697446ab9ee238285ec3fc56f618fcd4f69dd2
Dennis Yilmaz <dy.0x8@outlook.com>
2022-01-14 18:18:04 -0500

    TeleOp Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/teleop/runtime/MovementHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/teleop/runtime/MovementHandler.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.common.teleop.runtime;
 import static org.firstinspires.ftc.teamcode.common.teleop.OfficialTeleop.currentState;
 import static org.firstinspires.ftc.teamcode.common.teleop.OfficialTeleop.drive;
 import static org.firstinspires.ftc.teamcode.common.teleop.OfficialTeleop.gamepad;
+import static org.firstinspires.ftc.teamcode.common.teleop.OfficialTeleop.slowModeToggled;
+
+import android.util.Log;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
@@ -11,20 +14,110 @@ import org.firstinspires.ftc.teamcode.common.teleop.misc.Beta;
 
 public class MovementHandler {
 
+    // TODO: Add functionality
+    public enum RunType {
+        SLOW,
+        DEFAULT,
+        SPRINT
+    }
+
+    /**
+     * Interface Tag
+     */
+    public static String interfaceTag = "Movement Handler";
+
+    /**
+     * Current Driving Run Type
+     */
+    private static RunType currentRunType = RunType.DEFAULT;
+
     /**
      * Working Movement Handler Runtime
      */
     @Beta
     public Thread runtime = new Thread(() -> {
         while(currentState.equals(TeleOpRobotStates.RUNNING)) {
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -gamepad.getLeftY(),
-                            -gamepad.getLeftX(),
-                            -gamepad.getRightX()
-                    )
-            );
-            drive.update();
+            switch (currentRunType) {
+                case SLOW:
+                    if(!slowDriving.isAlive()) {
+                        if(defaultDriving.isAlive()) {
+                            defaultDriving.interrupt();
+                        }
+                        if(sprintDriving.isAlive()) {
+                            sprintDriving.interrupt();
+                        }
+                        slowDriving.start();
+                    }
+                    break;
+                case DEFAULT:
+                    if(!defaultDriving.isAlive()) {
+                        if(slowDriving.isAlive()) {
+                            slowDriving.interrupt();
+                        }
+                        if(sprintDriving.isAlive()) {
+                            sprintDriving.interrupt();
+                        }
+                        defaultDriving.start();
+                    }
+                    break;
+                case SPRINT:
+                    if(!sprintDriving.isAlive()) {
+                        if(slowDriving.isAlive()) {
+                            slowDriving.interrupt();
+                        }
+                        if(defaultDriving.isAlive()) {
+                            defaultDriving.interrupt();
+                        }
+                        sprintDriving.start();
+                    }
+                    break;
+                default:
+                    Log.e("Movement Handler", ": Internal Logic Error @runtime -> Drive Handler Checks");
+                    Log.e(interfaceTag, "Log Info: " + currentRunType);
+            }
         }
     });
+
+
+    private static Thread sprintDriving = new Thread(() -> {
+        drive.setWeightedDrivePower(
+                new Pose2d(
+                        -gamepad.getLeftY(),
+                        -gamepad.getLeftX(),
+                        -gamepad.getRightX()
+                )
+        );
+        drive.update();
+    });
+
+    private static Thread defaultDriving = new Thread(() -> {
+        drive.setWeightedDrivePower(
+                new Pose2d(
+                        -gamepad.getLeftY(),
+                        -gamepad.getLeftX(),
+                        -gamepad.getRightX()
+                )
+        );
+        drive.update();
+    });
+
+    private static Thread slowDriving = new Thread(() -> {
+
+    });
+
+    /**
+     * Lets you set the current run type
+     * @param runType the robot drive type
+     */
+    public static void setRunType(RunType runType) {
+        currentRunType = runType;
+    }
+
+    /**
+     * Gets the current movement run type
+     * @return current robot run type
+     */
+    public static RunType getRunType() {
+        return currentRunType;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/teleop/runtime/SlideHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/teleop/runtime/SlideHandler.java
@@ -11,12 +11,17 @@ import org.firstinspires.ftc.teamcode.common.teleop.Configuration;
 import org.sbs.bears.robotframework.enums.SlideTarget;
 
 public class SlideHandler {
-
+    //TODO make it work and push to github
     /**
      * Limits internal usage to one
      * @usage Threads check to see if this is already running
      */
     private static boolean running = false;
+
+    /**
+     * Interface Tag
+     */
+    public static String interfaceTag = "Slide Handler";
 
     /**
      * Slide Extended
@@ -34,7 +39,7 @@ public class SlideHandler {
     /**
      * One Carousel
      */
-    public Thread slideOneCarousel = new Thread(() -> {
+    public Thread slideOneCarousel = new Thread(() -> {//i like men
         if(!running) {
             running = true;
             slideController.extendDropRetract(SlideTarget.ONE_CAROUSEL);
