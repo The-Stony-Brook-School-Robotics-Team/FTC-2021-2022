b85c3253ffe2ce576f9f7a347a05bafb5c3fe651
William Tao <williamtao345@gmail.com>
2022-01-27 16:24:36 -0500

    Updated DuckSpinner Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/William/WheelControl/DuckCarouselController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/William/WheelControl/DuckCarouselController.java
@@ -31,13 +31,13 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class DuckCarouselController {
 
     //MAGICAL_CONSTANT should be between 0.30 to 0.40. Because of the lack of enough torque, the wheel actually never achieve the ideal acceleration.
-    public double MAGICAL_CONSTANT = 0.38;
-    public double MAGICAL_CONSTANT_TIME_OFFSET = 0.02;
+    public double MAGICAL_CONSTANT = 0.20;
+    public double MAGICAL_CONSTANT_TIME_OFFSET = 0.27;
 
     public double FIRST_STAGE_TIME_FLAG = -1;
-    public double FIRST_STAGE_TIME_INTERVAL = 1.5;
+    public double FIRST_STAGE_TIME_INTERVAL = 1.15;
     public double SECOND_STAGE_TIME_FLAG = -1;
-    public double SECOND_STAGE_TIME_INTERVAL = 0.1;
+    public double SECOND_STAGE_TIME_INTERVAL = 0.12;
     public double currentTime;
     public double initTime;
     public double runTime;
