ddab559ac3ecff3d87450462475754cf6e31d8f9
Michael Mihaley <71835811+michael-mih@users.noreply.github.com>
2022-01-31 15:27:02 -0500

    captive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Michael/Sensors/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandboxes/Michael/Sensors/ColorTest.java
@@ -2,34 +2,35 @@ package org.firstinspires.ftc.teamcode.Sandboxes.Michael.Sensors;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 
 @TeleOp(name = "Michael Color Test")
 public class ColorTest extends LinearOpMode
 {
-    public NormalizedColorSensor color;
+    public ColorRangeSensor redColor;
+    public ColorRangeSensor blueColor;
     float gain = 10;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        color = hardwareMap.get(NormalizedColorSensor.class, "color");
+        redColor = hardwareMap.get(ColorRangeSensor.class, "rc");
+        blueColor = hardwareMap.get(ColorRangeSensor.class, "bc");
 
         waitForStart();
 
         while (opModeIsActive()) {
-            color.setGain(gain);
+            telemetry.addData("red Red: ", redColor.red());
+            telemetry.addData("red Blue: ", redColor.blue());
+            telemetry.addData("red Green: ", redColor.green());
+            telemetry.addData("red Alpha: ", redColor.alpha());
+            telemetry.addLine();
+            telemetry.addData("blue Red: ", blueColor.red());
+            telemetry.addData("blue Blue: ", blueColor.blue());
+            telemetry.addData("blue Green: ", blueColor.green());
+            telemetry.addData("blue Alpha: ", blueColor.alpha());
 
-            if(gamepad1.a){gain+=.005;}
-            else if(gamepad1.b){gain-=.005;}
-            else if(gamepad1.dpad_up){gain+=1;}
-            else if(gamepad1.dpad_down){gain-=1;}
-
-            telemetry.addData("Gain: ", color.getGain());
-            telemetry.addData("Red: ", color.getNormalizedColors().red);
-            telemetry.addData("Blue: ", color.getNormalizedColors().blue);
-            telemetry.addData("Green: ", color.getNormalizedColors().green);
-            telemetry.addData("Alpha: ", color.getNormalizedColors().alpha);
             telemetry.update();
         }
     }
