a86bb68620ffcfcc97fb6ccc125a6ecfe531f2b7
Bears-Console <90263953+Bears-Console@users.noreply.github.com>
2022-02-04 13:33:20 -0500

    add testing program, also fixed dumper positions

--- TeamCode/src/main/java/org/sbs/bears/robotframework/controllers/SlideController.java
+++ TeamCode/src/main/java/org/sbs/bears/robotframework/controllers/SlideController.java
@@ -678,10 +678,10 @@ public class SlideController {
     // dumper servo
 /*    double dumperPosition_DUMP = .91;
     double dumperPosition_HOLDBLOCK = 0;
-*/    public double dumperPosition_CLOSED = .45;  // remeasured on jan 31 at 16h08
-    public double dumperPosition_READY = .2;
-    public double dumperPosition_EJECT = 0.0025;
-    public double dumperPosition_RETRACTING = .75;
+*/    public double dumperPosition_CLOSED = 0.33;  // remeasured on jan 31 at 16h08
+    public double dumperPosition_READY = 0.53;
+    public double dumperPosition_EJECT = 0.74;
+    public double dumperPosition_RETRACTING = 0.05;
 
     // slide motor
     int slideMotorPosition_PARKED = 5;
