71bf637d7e60814f3c75da2c0df4adee6b03cb1b
Dennis Yilmaz <dy.0x8@outlook.com>
2022-02-08 17:19:26 -0500

    Refactor

--- TeamCode/src/main/java/org/sbs/bears/coyote/servopkg/ServoMotionSegment.java
+++ TeamCode/src/main/java/org/sbs/bears/coyote/servopkg/ServoMotionSegment.java
@@ -2,68 +2,50 @@ package org.sbs.bears.coyote.servopkg;
 
 public class ServoMotionSegment {
 
-    private double iniAng;
-    private double finiAng;
+    private double angle;
     private int dt;
     private double v;
 
     /**
      * Motion Segment
-     * @param iniAng
-     * @param finalAng
+     * @param angle
      * @param dt
      * @param v
      */
-    public ServoMotionSegment(double iniAng, double finalAng, int dt, double v){
-        this.iniAng = iniAng;
-        this.finiAng = finalAng;
+    public ServoMotionSegment(double angle, int dt, double v){
+        this.angle = angle;
         this.dt = dt;
         this.v = v;
     }
 
-    public double getIniAng() {
-        return iniAng;
-    }
-
-    public double getFiniAng() {
-        return finiAng;
+    public double getAngle() {
+        return angle;
     }
 
     public int getDt() {
         return dt;
     }
 
-    public double getV() {
-        return v;
-    }
-
-    public double setIniAng(double add) {
-        return iniAng;
-    }
-
-    public double setFiniAng(double add) {
-        return finiAng;
+    public double setAngle(double angle) {
+        this.angle = angle;
+        return angle;
     }
 
     public int setDt(int add) {
         return dt;
     }
 
-    public double setV(double add) {
-        return v;
-    }
 
     public ServoMotionSegment add(ServoMotionSegment segment) {
         return new ServoMotionSegment(
-                this.iniAng + segment.iniAng,
-                this.finiAng + segment.finiAng,
+                this.angle + segment.angle,
                 this.dt + segment.dt,
                 this.v + segment.v);
     }
 
     public String toString()
     {
-        return "ServoMotionSegment with iniAngle " + iniAng + ", finAng " + finiAng + ", vel " + v + ", and ∆t " + dt;
+        return "ServoMotionSegment with iniAngle " + angle + ", finAng " + angle + ", vel " + v + ", and ∆t " + dt;
     }
 
 
