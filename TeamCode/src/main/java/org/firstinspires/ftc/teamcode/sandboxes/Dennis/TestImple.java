9f2de6f7b7e760d475d036be15bc188f1281adfa
Dennis Yilmaz <dy.0x8@outlook.com>
2022-02-08 17:32:21 -0500

    Update TestImple.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/Dennis/TestImple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/Dennis/TestImple.java
@@ -4,16 +4,10 @@ package org.firstinspires.ftc.teamcode.sandboxes.Dennis;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.sbs.bears.coyote.servopkg.ServoMotionProfile;
-import org.sbs.bears.coyote.servopkg.ServoMotionSegment;
-
-import java.util.ArrayList;
-import java.util.List;
-
 @TeleOp(name = "A - Coyote Servo Package")
 public class TestImple extends LinearOpMode {
 
-    
+
 
     @Override
     public void runOpMode() throws InterruptedException {
