e474f4582d6335f26d109dd8395d8fe46b6e6dc0
Astro Kid <71262643+MDNich@users.noreply.github.com>
2022-02-16 16:02:28 -0500

    Create AutonomousBlueFullTurn.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousBlueFullTurn.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.common.autonomous;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.sbs.bears.robotframework.controllers.OpenCVController;
+
+@Autonomous (name = "A - Auton (Blue Full Turn)")
+public class AutonomousBlueFullTurn extends LinearOpMode {
+    AutonomousBrainTurn brain;
+    boolean qA = false;
+    boolean qContinue = false;
+    boolean masterQContinue = true;
+    public static Gamepad gamepad;
+
+    @Override
+    public void runOpMode()
+    {
+        OpenCVController.isDuck = false;
+        brain = new AutonomousBrainTurn(hardwareMap,telemetry,AutonomousMode.BlueFull);
+        Log.d("Auton BF","Init Complete");
+        msStuckDetectLoop = Integer.MAX_VALUE;
+        gamepad = gamepad1;
+
+        waitForStart();
+
+        brain.start();
+
+        autonBrainExecutor.start();
+        while(opModeIsActive() && !isStopRequested())
+        {
+            telemetry.addData("majorState", brain.majorState);
+            telemetry.addData("minorState", brain.minorState);
+            telemetry.addData("CameraReading", brain.heightFromDuck);
+            telemetry.addData("DepositHeight", brain.iniTarget);
+            telemetry.update();
+        }
+        // stop requested
+        masterQContinue = false; // master switch
+        autonBrainExecutor.interrupt();
+        try {
+            autonBrainExecutor.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        brain.majorState = AutonomousBrainTurn.MajorAutonomousState.FINISHED;
+        brain.minorState = AutonomousBrainTurn.MinorAutonomousState.STOPPED;
+        requestOpModeStop();
+        stop();
+    }
+
+
+    Thread autonBrainExecutor = new Thread(()->{
+        while(opModeIsActive()&& !isStopRequested()){
+            if(!masterQContinue) {break;}
+            brain.doStateAction();
+            if(brain.majorState.equals(AutonomousBrainTurn.MajorAutonomousState.FINISHED)) { requestOpModeStop(); }
+            if(!masterQContinue) { break; }
+
+        }
+    });
+
+}
