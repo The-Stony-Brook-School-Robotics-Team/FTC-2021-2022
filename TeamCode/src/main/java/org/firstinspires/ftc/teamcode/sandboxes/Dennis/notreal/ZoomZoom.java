97b5789be528380d97820f74d2955232a72f9171
Dennis Yilmaz <dy.0x8@outlook.com>
2022-02-22 18:25:51 -0500

    sdfdsdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/Dennis/notreal/ZoomZoom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/Dennis/notreal/ZoomZoom.java
@@ -1,57 +1,139 @@
 package org.firstinspires.ftc.teamcode.sandboxes.Dennis.notreal;
 
+import android.util.Log;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.util.NanoClock;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.common.autonomous.AutonomousMode;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.sbs.bears.robotframework.controllers.DuckCarouselController;
 import org.sbs.bears.robotframework.controllers.IntakeControllerBlue;
 import org.sbs.bears.robotframework.controllers.IntakeControllerRed;
+import org.sbs.bears.robotframework.controllers.OpenCVController;
 import org.sbs.bears.robotframework.controllers.SlideController;
+import org.sbs.bears.robotframework.enums.SlideTarget;
+import org.sbs.bears.robotframework.enums.TowerHeightFromDuck;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * This is not real, wake up
  */
 @Autonomous(name = "Z - Zoom Zoom Mover (ONG!)")
-public class ZoomZoom extends OpMode {
+public class ZoomZoom extends LinearOpMode {
 
+    /**
+     * ZoomZoom Interface Tag
+     */
+    private static String interfaceTag = "ZoomZoom";
+
+    /**
+     * The ZoomZoom States for The Robot
+     */
     private enum ZoomZoomStates {
         IDLE,
         DEPOSIT,
         PREP,
-        PICKUP
+        PICKUP,
+        STUCK
     }
 
-    private ZoomZoomStates CurrentZoomZoomState = ZoomZoomStates.IDLE;
+    /**
+     * Current ZoomZoom State
+     */
+    private static ZoomZoomStates CurrentZoomZoomState = ZoomZoomStates.IDLE;
 
     /**
      * Controllers
      */
-    private IntakeControllerBlue blueIntakeController;
-    private IntakeControllerRed redIntakeController;
-    private SlideController slideController;
+    public static OpenCVController openCVController;
+    public static IntakeControllerBlue blueIntakeController;
+    public static IntakeControllerRed redIntakeController;
+    public static SlideController slideController;
+    public static DuckCarouselController duckCarouselController;
 
     /**
      * RoadRunner Objects
      */
-    private SampleMecanumDrive drive;
+    public static SampleMecanumDrive drive;
+
+    /**
+     * Internal Management
+     */
+    public static Pose2d initialDepositPosition;
+    public static TowerHeightFromDuck initialDepositHeight;
+    public static SlideTarget initialSlideTarget;
 
     @Override
-    public void init() {
+    public void runOpMode() {
+        /**
+         * Initialize RoadRunner
+         */
+        drive = new SampleMecanumDrive(hardwareMap);
+
         /**
          * Initialize Controllers
          */
-        // blueIntakeController = new IntakeControllerBlue(this.hardwareMap);
+        slideController = new SlideController(hardwareMap,telemetry);
+        blueIntakeController = new IntakeControllerBlue(hardwareMap, slideController.dumperServo, telemetry);
+        redIntakeController = new IntakeControllerRed(hardwareMap, telemetry);
+        duckCarouselController = new DuckCarouselController(hardwareMap, telemetry);
+        openCVController = new OpenCVController(hardwareMap, telemetry, AutonomousMode.BlueFull);
 
+        waitForStart();
+
+        depositInitialBlock();
+
+
+
+        /**
+         * While a stop is not request && the timer is under 25 seconds
+         */
+        while(!isStopRequested() && NanoClock.system().seconds() < 25) {
+
+
+
+        }
     }
 
-    @Override
-    public void loop() {
+    public static void depositInitialBlock() {
+        readCamera();
+        // Choose the deposit height
+        switch (initialDepositHeight) {
+            case ONE:
+                initialSlideTarget = SlideTarget.BOTTOM_DEPOSIT;
+                initialDepositPosition = ZoomZoomConfiguration.allianceDepositPosition_BOTTOM;
+                break;
+            case TWO:
+                initialSlideTarget = SlideTarget.MID_DEPOSIT;
+                initialDepositPosition = ZoomZoomConfiguration.allianceDepositPosition_MIDDLE;
+                break;
+            case THREE:
+                initialSlideTarget = SlideTarget.TOP_DEPOSIT;
+                initialDepositPosition = ZoomZoomConfiguration.allianceDepositPosition_TOP;
+                break;
+        }
+        // Go to the deposit position
+        drive.followTrajectory(
+                drive.trajectoryBuilder(drive.getPoseEstimate())
+                        .lineToSplineHeading(initialDepositPosition)
+                        .build()
+        );
 
+        // Extend the slide
+        slideController.extendDropRetract(initialSlideTarget);
     }
 
-    @Override
-    public void stop() {
-
+    public static void readCamera() {
+        // Read from camera
+        readCamera();
+        initialDepositHeight = openCVController.getWhichTowerHeight();
+        openCVController.shutDown();
     }
 
 }
