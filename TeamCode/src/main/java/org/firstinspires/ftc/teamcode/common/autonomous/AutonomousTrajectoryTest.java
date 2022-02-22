9917c8b29dc886a6d1c43d642ef36ad11e9b8d4e
William Tao <williamtao345@gmail.com>
2022-02-22 10:42:43 -0500

    Update AutonomousTrajectoryTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousTrajectoryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousTrajectoryTest.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode.common.autonomous;
 
-import com.acmerobotics.roadrunner.util.NanoClock;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.sbs.bears.robotframework.controllers.OpenCVController;
 import org.sbs.bears.robotframework.controllers.RoadRunnerController;
