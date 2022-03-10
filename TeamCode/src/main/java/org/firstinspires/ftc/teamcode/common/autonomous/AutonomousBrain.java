8df42e2259420b5591cba879b1a9d4dac65eaff4
Bears-Console <90263953+Bears-Console@users.noreply.github.com>
2022-03-09 20:43:32 -0500

    STABLE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousBrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/autonomous/AutonomousBrain.java
@@ -453,7 +453,7 @@ public class AutonomousBrain {
     public static Pose2d startPositionRed = new Pose2d(startPositionRedX,startPositionRedY,Math.toRadians(startPositionRedH)); // TODO may need to remeasure
     public static Pose2d warehousePickupPositionBlue = new Pose2d(43,70,0);
     public static Pose2d warehousePickupPositionRed = new Pose2d(43,-70,-Math.PI);
-    public static Pose2d depositPositionBlueNoTurn = new Pose2d(-11,75,0);
+    public static Pose2d depositPositionBlueNoTurn = new Pose2d(-18,75,0);
     public static Pose2d depositPositionRedNoTurn = new Pose2d(-22,-75,-Math.PI);
     public static Pose2d depositPositionAllianceBlueTOP = new Pose2d(5.58,64.47, -Math.toRadians(30)); //55
     public static Pose2d depositPositionAllianceRedTOP = new Pose2d(5.58,-64.47, -Math.toRadians(150)); //55
