320f952c66b9e16a825e67d9cf9d13f0db367691
Michael Mihaley <71835811+michael-mih@users.noreply.github.com>
2022-04-15 13:49:06 -0400

    Create CustomTankLocalizer.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CustomTankLocalizer.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.acmerobotics.roadrunner.drive.TankDrive;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.localization.Localizer;
+
+public class CustomTankLocalizer implements Localizer {
+
+    SampleTankDrive drive;
+    boolean useExtHeading = false;
+    
+    public CustomTankLocalizer(SampleTankDrive drive, boolean useExtHeading)
+    {
+        this.drive = drive;
+    }
+
+    @NonNull
+    @Override
+    public Pose2d getPoseEstimate() {
+        return null;
+    }
+
+    @Override
+    public void setPoseEstimate(@NonNull Pose2d pose2d) {
+
+    }
+
+    @Nullable
+    @Override
+    public Pose2d getPoseVelocity() {
+        return null;
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
